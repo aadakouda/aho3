56a79ad279e0faf94a9c9d876c1283e0a794fa2e
aadakouda <kurottsu@gmail.com>
2022-06-05 23:39:06 +0900

    first commit

---
+++ python/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi import Query
+
+app = FastAPI()
+
+
+def check_aho(number):
+    """
+    アホかどうかチェックする関数
+    """
+    return number%3 == 0 or '3' in str(number)
+
+
+def next_number(number):
+    """
+    つぎの数字を返す関数
+    """
+    return number+1 if number < 40 else 1
+
+
+@app.get('/')
+async def aho3(number: int = Query(default=1, ge=1, le=40)):
+    """
+    3の倍数と3がつく数字のときアホになるAPI
+    """
+    response_json = {
+        'number': number,
+        'aho': check_aho(number),
+        'next_number': next_number(number),
+    }
+    return response_json
